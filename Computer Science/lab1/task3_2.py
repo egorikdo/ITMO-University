8eed480693afbafd0c48c0bab8eaf09e64f6ebf3
Dorovskii Egor <135843688+egorikdo@users.noreply.github.com>
2024-11-18 00:49:58 +0300

    Create task3_2.py

---
+++ Computer Science/lab1/task3_2.py
@@ -0,0 +1,22 @@
+import re
+xml_file = open('YOURFILE.xml', 'r')
+yaml_file = open('YOURFILEFORPARSE.txt', 'w')
+
+flag = 0
+counter = 0
+for line in xml_file:
+    xml_string = line
+    yaml_string = ''
+    patt = re.findall(r'\s{0,}<\w{1,}>', xml_string)
+    if patt:
+        name = patt[0]
+        name = name.replace('<', '')
+        name = name.replace('>', '')
+        if name[0] == '\t':
+            num = name.count('\t')
+            if num != 0:
+                name = num * '\t' + '-' + name[num:]
+        yaml_string += name + ':'
+    patt = re.findall(r'>(.*?)<', xml_string)
+    yaml_string += (patt[0] if patt else '')
+    yaml_file.write(yaml_string + '\n' if yaml_string else '')
